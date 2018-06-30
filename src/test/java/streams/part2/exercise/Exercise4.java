188d7218f86170e45d7806702059bba7451c278b
Viktoria Naboychenko <viktoriia_naboichenko@epam.com>
2018-06-30 21:58:32 +0300

    Streams part 2 exercise 4 done.

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -1,24 +1,45 @@
 package streams.part2.exercise;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.counting;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise4 {
 
     /**
-     * Выбирает из текста наиболее часто встречающиеся слова.
-     * Подсчет слов выполняется без учета их регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово.
-     * Если некоторые слова имеют одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
+     * Выбирает из текста наиболее часто встречающиеся слова. Подсчет слов выполняется без учета их
+     * регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово. Если некоторые слова имеют
+     * одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
+     *
      * @param text Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
-     * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо отобрать.
+     * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо
+     * отобрать.
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+
+        Comparator<Entry<String, Long>> comparingByValue = (entry1, entry2) -> Long
+            .compare(entry2.getValue(), entry1.getValue());
+        Comparator<Entry<String, Long>> comparingByKey = Entry.comparingByKey();
+
+        Map<String, Long> frequencyCounted =
+            Arrays.stream(text.split(" "))
+                .collect(Collectors.groupingBy(String::toLowerCase, counting()));
+
+        return frequencyCounted.entrySet()
+            .stream()
+            .sorted(comparingByValue.thenComparing(comparingByKey))
+            .limit(numberWords)
+            .map(Entry::getKey)
+            .collect(Collectors.toList());
     }
 
     @Test
@@ -32,14 +53,17 @@ public class Exercise4 {
 
     @Test
     public void test2() {
-        String source = "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at "
-                      + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
-                      + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
-                      + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
-                      + "Integer vel odio nec mi tempor dignissim";
+        String source =
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at "
+                + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
+                + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
+                + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
+                + "Integer vel odio nec mi tempor dignissim";
 
         List<String> result = getFrequentlyOccurringWords(source, 10);
 
-        assertEquals(Arrays.asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor", "eget", "elit", "mi"), result);
+        assertEquals(Arrays
+            .asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor",
+                "eget", "elit", "mi"), result);
     }
 }
